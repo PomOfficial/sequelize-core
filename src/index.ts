1a461787e3d837098009e0f2516e484308a5e19d
Jim Bulkowski <jim.b@paperelectron.com>
2019-04-26 12:43:01 -0400

    Rename plugin internally

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ import Sequelize from 'sequelize'
 
 export const Plugin = CreatePlugin('merge')
 .configuration({
-  name: 'Core',
+  name: 'Sequelize-Core',
   injectableParam: 'SQL'
 })
 .hooks({
