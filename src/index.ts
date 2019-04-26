d0b13ed3a616f530992e80037d8f7de2f34feef6
Jim Bulkowski <jim.b@paperelectron.com>
2019-04-26 09:30:29 -0400

    Initial

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+/**
+ * @file index
+ * @author Jim Bulkowski <jim.b@paperelectron.com>
+ * @project sequelize-core
+ * @license MIT {@link http://opensource.org/licenses/MIT}
+ */
+
+import {CreatePlugin} from "@pomegranate/plugin-tools";
+import Sequelize from 'sequelize'
+
+export const Plugin = CreatePlugin('merge')
+.configuration({
+  name: 'Core',
+  injectableParam: 'SQL'
+})
+.hooks({
+  load: () => {
+    return {Sequelize: Sequelize}
+  }
+})
